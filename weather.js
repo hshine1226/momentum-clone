8742582e7c392e1e0e5f91bf04a21fa2fb633090
hshine1226 <hshine1226@gmail.com>
2020-04-24 21:30:56 +0900

    기능: 날씨 API를 이용해서 날씨 정보를 받아와 출력하는 기능 날씨 API를 이용해서 data를 얻어옴(fetch) then으로 fetch가 완료될 때까지 기다렸다가 완료되면 정보를 화면에 출력함

--- weather.js
+++ weather.js
@@ -1,7 +1,31 @@
+const weather = document.querySelector(".js-weather");
 /* 날씨 정보를 받아올 API KEY 
-https://home.openweathermap.org/api_keys*/
+https://home.openweathermap.org/api_keys
+API(Application Programming Interface)
+다른 서버로부터 손쉽게 데이터를 가져올 수 있는 수단*/
 const API_KEY = "589e7e5001900885992abe3d6d58049e";
 const COORDS = "coords";
+function getWeather(lat, lon) {
+  /*weather data를 얻어올 때는 fetch를 사용한다.
+  https://openweathermap.org/current#data 에서 Units format을 확인해보면
+  units를 확인할 수 있다. 
+  나는 온도가 섭씨로 표시되기 원하기 때문에 units=metric을 fetch 마지막에 추가한다.
+  then은 fetch가 끝나길 기다렸다가 함수를 실행하게 해준다. 이 부분이 이해가 안가면
+  javascript 코스를 수강하자!
+  */
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+      console.log(json);
+    });
+}
 function saveCoords(coordsObject) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObject));
 }
@@ -19,6 +43,7 @@ function handleGeoSuccess(position) {
     longitude,
   };
   saveCoords(coordsObject);
+  getWeather(latitude, longitude);
 }
 function handleGeoError() {
   console.log("Can't access geo location");
@@ -33,7 +58,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-    //getWeather();
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 function init() {
